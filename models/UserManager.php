097956287d0cc5977ddafb0db19d091b67015768
NovySoyuz <florentcabassut@orange.fr>
2024-05-27 14:53:08 +0200

    Simple login/logout system

--- models/UserManager.php
+++ models/UserManager.php
@@ -14,3 +14,22 @@ function GetAllUsers()
   $response = $PDO->query("SELECT * FROM user ORDER BY nickname ASC");
   return $response->fetchAll();
 }
+
+function GetUserIdFromUserAndPassword($username, $password)
+{
+  global $PDO;
+
+  $sqlQuery = 'SELECT * FROM user WHERE nickname = :nickname';
+  $pdo_prepare = $PDO->prepare($sqlQuery);
+  $pdo_prepare->execute(['nickname' => $username]);
+  $user = $pdo_prepare->fetch();
+
+  var_dump($user);
+
+  if ($user && $password === $user['password']) {
+    $_SESSION['userId'] = $user['id'];
+    return $user['id'];
+  } else {
+    return -1;
+  }
+}
